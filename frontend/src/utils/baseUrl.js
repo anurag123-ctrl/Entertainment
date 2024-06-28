43058b62aa698a30f52a1335853e3f9a00545983
Anurag Kashyap <84828384+anurag123-ctrl@users.noreply.github.com>
2024-06-28 23:42:45 +0530

    Update baseUrl.js

--- frontend/src/utils/baseUrl.js
+++ frontend/src/utils/baseUrl.js
@@ -1,3 +1,3 @@
-const baseUrl = "https://entertainment-web-app-3adk.onrender.com"
+const baseUrl = "https://entertainment-web-app-3adk.onrender.com/" || "http://localhost:8000/api"
 
 export default baseUrl;
